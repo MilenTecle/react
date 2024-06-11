39516bfd6db6a0e4851e70ca0373b9e29d47cc7c
Milen Tecle <m_tecle@hotmail.com>
2024-06-11 19:05:34 +0000

    create react myy app

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import StatefulGreeting from "./components/StatefulGreeting";
-import HooksCounter from "./components/HooksCounter";
+
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting name="John" />
-      {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
+      <h1>It works!</h1>
     </div>
   );
 }
