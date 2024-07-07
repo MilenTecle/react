d0847e3e0387de54246e25966afff8f94bd40632
Milen Tecle <m_tecle@hotmail.com>
2024-07-07 19:35:33 +0000

    Add a clickhandler

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component{
-render () {
-    return <h1>{this.props.greeting}</h1>
+    constructor(props) {
+        super(props);
+        this.state = {
+             introduction: "Hello",
+             buttonText: "Exit",
+        };  
+    }
+
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter"
+        }, () =>{
+        console.log('new state',this.state.introduction);
+        console.log('new state',this.state.buttonText);
+        });
+        console.log(this.state.introduction);
+        console.log(this.state.buttonText);
+    }
+
+    render () {
+        return(
+        <div>
+            <h1>{this.state.introduction} {this.props.greeting}</h1>
+            <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+
+        </div>
+        )
     }
 }
 
