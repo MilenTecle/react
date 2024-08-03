f77c4fa995227178113bfdfff44507de4dd0dcdb
Milen Tecle <m_tecle@hotmail.com>
2024-08-03 07:58:25 +0000

    UseState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import SearchBar from "./components/SearchBar"; 
+import UseStateWithArrays from "./components/UseStateWithArrays";
+
 
 function App() {
   return <div className="App">
-      <SearchBar />
+      <UseStateWithArrays/>
     </div>;
 }
 
