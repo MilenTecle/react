fbea7d0ef1741355a83104ef589a51ee185db4c8
Milen Tecle <m_tecle@hotmail.com>
2024-08-08 19:07:43 +0000

    USeEffect hooks and cleanup function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import UseStateWithObjects from "./components/UseStateWithObjects";
+import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
 
 
 function App() {
   return <div className="App">
-      <UseStateWithObjects/>
+      <UseEffectCounterContainer />
     </div>;
 }
 
