89c1bd49c748c423efef03d9bf6396f0c5a2a19b
Milen Tecle <m_tecle@hotmail.com>
2024-08-19 10:34:30 +0000

    Converting POST using hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import HTTPhooks from "./components/HTTPhooks";
 import HTTPPost from "./components/HTTPPost";
 
 
 function App() {
   return <div className="App">
-      <HTTPPost />
+      <HTTPhooks />
     </div>;
 }
 
